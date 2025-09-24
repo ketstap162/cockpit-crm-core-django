253b991ef86d9b006d4235608851dab0a9f91e49
ketstap162 <zp162162@gmail.com>
2025-09-24 09:12:50 +0300

    Register entities models in entities/admin.py. Make migration for Entity & EntityType models

--- entities/admin.py
+++ entities/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import EntityType, Entity
 
-# Register your models here.
+
+@admin.register(EntityType)
+class EntityTypeAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "uuid")
+    search_fields = ("code", "name")
+    ordering = ("code",)
+    readonly_fields = ("uuid",)
+
+
+@admin.register(Entity)
+class EntityAdmin(admin.ModelAdmin):
+    list_display = (
+        "display_name",
+        "entity_type",
+        "uuid",
+        "is_current",
+        "valid_from",
+        "valid_to",
+    )
+    list_filter = ("entity_type", "is_current")
+    search_fields = ("display_name", "uuid")
+    ordering = ("display_name",)
+    autocomplete_fields = ("entity_type",)
+    readonly_fields = ("uuid", "valid_from", "valid_to")
+
+    actions = ["close_selected"]
+
+    def close_selected(self, request, queryset):
+        """
+        Закриває поточну версію обраних Entity.
+        """
+        for obj in queryset:
+            obj.close()
+        self.message_user(request, "Selected entities have been closed.")
+    close_selected.short_description = "Close selected current versions"
