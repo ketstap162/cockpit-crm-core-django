b3116514fb0911e9de589d2ceec798e8cadabdfa
ketstap162 <zp162162@gmail.com>
2025-09-27 18:23:34 +0300

    Update Entity models: add constraints & indexes

--- entities/models.py
+++ entities/models.py
@@ -1,4 +1,9 @@
+from django.contrib.postgres.constraints import ExclusionConstraint
+from django.contrib.postgres.fields import RangeOperators
+from django.contrib.postgres.indexes import GinIndex
+from django.contrib.postgres.search import SearchVectorField
 from django.db import models
+from django.db.models import F, Func, UniqueConstraint, Value, DateTimeField, Q, Index
 
 from core.models.base import BaseModel
 from core.models.uuid import UUIDModel, get_uuid_indexes
@@ -26,27 +31,52 @@ class EntityType(UUIDModel, BaseModel):
 class Entity(SCD2BaseModel):
     # Fields
     display_name = models.CharField(max_length=255)
-
-    # Tech attributes
-    detection_fields = ["display_name"]
-    allow_hash_diff = False
+    search_vector = SearchVectorField(null=True)
 
     # Keys
     entity_type = models.ForeignKey(
         EntityType,
         on_delete=models.PROTECT,
         related_name="entities",
-        db_column="entity_uuid"
+        db_column="entity_type_id"
     )
+
+    # Tech attributes (Not fields)
+    detection_fields = ["display_name"]
+    allow_hash_diff = False
 
     class Meta:
         verbose_name = "Entity"
         verbose_name_plural = "Entities"
         indexes = [
             *get_scd2_indexes("entity"),
-            models.Index(fields=["display_name"]),
+            GinIndex(fields=["search_vector"], name="entity_search_gin")
         ]
         constraints = [
+            UniqueConstraint(
+                fields=["uuid"],
+                condition=Q(is_current=True),
+                name="uniq_current_entity_per_uuid"
+            ),
+            ExclusionConstraint(
+                name="exclude_overlapping_entity_versions",
+                expressions=[
+                    (F("uuid"), "="),
+                    (
+                        Func(
+                            F("valid_from"),
+                            Func(
+                                F("valid_to"),
+                                # Value("infinity", output_field=DateTimeField()),
+                                template="COALESCE(%(expressions)s, 'infinity')",
+                                function="COALESCE"
+                            ),
+                            function="TSTZRANGE"
+                        ),
+                        RangeOperators.OVERLAPS,
+                    ),
+                ],
+            ),
             *get_scd2_constraint_list("entity"),
         ]
 
@@ -74,18 +104,35 @@ class EntityDetail(SCD2BaseModel):
         verbose_name_plural = "Entity Details"
         indexes = [
             *get_scd2_indexes("entity_detail"),
-            models.Index(fields=['detail_code']),
+            Index(fields=['detail_code']),
         ]
         constraints = [
-            models.UniqueConstraint(
+            UniqueConstraint(
                 fields=['entity_uuid', 'detail_code'],
                 condition=models.Q(is_current=True),
                 name='unique_current_entity_detail'
             ),
-            models.UniqueConstraint(
+            UniqueConstraint(
                 fields=['entity_uuid'],
                 condition=models.Q(is_current=True),
                 name='unique_current_per_entity_uuid'
+            ),
+            ExclusionConstraint(
+                name="exclude_overlapping_entity_detail",
+                expressions=[
+                    (F("entity_uuid"), "="),
+                    (F("detail_code"), "="),
+                    (
+                        Func(
+                            F("valid_from"),
+                            Func(
+                                F("valid_to"),
+                                template="COALESCE(%(expressions)s, 'infinity')",
+                                function="COALESCE"), function="TSTZRANGE"
+                        ),
+                        RangeOperators.OVERLAPS,
+                    ),
+                ],
             ),
             *get_scd2_constraint_list("entity_detail"),
         ]
